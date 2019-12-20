d7404be0acce9f407ed09adddf59e500166989f6
a <a@out.com>
2019-12-20 17:51:39 +0800

    redis

--- redis-test/index.js
+++ redis-test/index.js
@@ -1,4 +1,20 @@
 const redis = require('redis')
 
 // 创建客户端
-const redisClient
+const redisClient = redis.createClient(6379,'127.0.0.1')
+redisClient.on('error', err => {
+    console.log(err)
+})
+
+// 测试
+redisClient.set('myname','zhangsan2222',redis.print)
+redisClient.get('myname', (err, val) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('val:',val)
+
+    // 退出
+    redisClient.quit()
+})
